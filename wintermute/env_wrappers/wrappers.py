689eea244b0fcb3d561ef98fcd3f608a8cdcbfee
Florin Gogianu <florin.gogianu@gmail.com>
2018-04-10 16:00:43 +0300

    Add proper DeepMind ALE wrapper.

--- wintermute/env_wrappers/wrappers.py
+++ wintermute/env_wrappers/wrappers.py
@@ -1,4 +1,5 @@
 """ Classes wrapping the OpenAI Gym.
+    Some / most of them ar shoplifted from OpenAI/baselines
 """
 from collections import deque
 
@@ -15,6 +16,7 @@ __all__ = ["TorchWrapper", "SqueezeRewards", "FrameStack", "DoneAfterLostLife",
 
 
 class TorchWrapper(gym.ObservationWrapper):
+    """ From numpy to torch. """
     def __init__(self, env):
         super().__init__(env)
 
@@ -25,7 +27,68 @@ class TorchWrapper(gym.ObservationWrapper):
         return torch.from_numpy(o).permute(2, 0, 1).unsqueeze(0).contiguous()
 
 
+class NoopResetEnv(gym.Wrapper):
+    """Sample initial states by taking random number of no-ops on reset.
+    No-op is assumed to be action 0.
+    """
+    def __init__(self, env, noop_max=30):
+        gym.Wrapper.__init__(self, env)
+        self.noop_max = noop_max
+        self.override_num_noops = None
+        self.noop_action = 0
+        assert env.unwrapped.get_action_meanings()[0] == 'NOOP'
+
+    def reset(self, **kwargs):
+        """ Do no-op action for a number of steps in [1, noop_max]."""
+        self.env.reset(**kwargs)
+        if self.override_num_noops is not None:
+            noops = self.override_num_noops
+        else:
+            noops = self.unwrapped.np_random.randint(1, self.noop_max + 1)
+        assert noops > 0
+        obs = None
+        for _ in range(noops):
+            obs, _, done, _ = self.env.step(self.noop_action)
+            if done:
+                obs = self.env.reset(**kwargs)
+        return obs
+
+    def step(self, ac):
+        return self.env.step(ac)
+
+
+class MaxAndSkipEnv(gym.Wrapper):
+    """Return only every `skip`-th frame"""
+    def __init__(self, env, skip=4):
+        gym.Wrapper.__init__(self, env)
+        # most recent raw observations (for max pooling across time steps)
+        self._obs_buffer = np.zeros((2,) + env.observation_space.shape,
+                                    dtype=np.uint8)
+        self._skip = skip
+
+    def reset(self, **kwargs):
+        return self.env.reset(**kwargs)
+
+    def step(self, action):
+        """Repeat action, sum reward, and max over last observations."""
+        total_reward = 0.0
+        done = None
+        for i in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            if i == self._skip - 2: self._obs_buffer[0] = obs
+            if i == self._skip - 1: self._obs_buffer[1] = obs
+            total_reward += reward
+            if done:
+                break
+        # Note that the observation on the done=True frame
+        # doesn't matter
+        max_frame = self._obs_buffer.max(axis=0)
+
+        return max_frame, total_reward, done, info
+
+
 class SqueezeRewards(gym.RewardWrapper):
+    """ Return only the sign of the reward at each step. """
     def __init__(self, env):
         super().__init__(env)
         print("[Reward Wrapper] for clamping rewards to -+1")
@@ -35,6 +98,8 @@ class SqueezeRewards(gym.RewardWrapper):
 
 
 class TransformObservations(gym.ObservationWrapper):
+    """ Applies the given transformations on the observations.
+    """
     def __init__(self, env, transformations):
         super().__init__(env)
         self.transformations = transformations
@@ -45,14 +110,14 @@ class TransformObservations(gym.ObservationWrapper):
             except AttributeError:
                 pass
 
-    def observation(self, o):
+    def observation(self, observation):
         for t in self.transformations:
-            o = t.transform(o)
-        return o
+            observation = t.transform(observation)
+        return observation
 
     def _reset(self):
-        o = self.env.reset()
-        return self.observation(o)
+        observation = self.env.reset()
+        return self.observation(observation)
 
     def __str__(self):
         r = ""
@@ -62,9 +127,8 @@ class TransformObservations(gym.ObservationWrapper):
 
 
 class FrameStack(gym.Wrapper):
+    """Stack k last frames. """
     def __init__(self, env, k):
-        """Stack k last frames.
-        """
         super().__init__(env)
         self.k = k
         self.frames = deque([], maxlen=k)
@@ -74,14 +138,14 @@ class FrameStack(gym.Wrapper):
                                                 dtype=np.uint8)
 
     def reset(self):
-        ob = self.env.reset()
+        observation = self.env.reset()
         for _ in range(self.k):
-            self.frames.append(ob)
+            self.frames.append(observation)
         return self._get_ob()
 
     def step(self, action):
-        ob, reward, done, info = self.env.step(action)
-        self.frames.append(ob)
+        observation, reward, done, info = self.env.step(action)
+        self.frames.append(observation)
         return self._get_ob(), reward, done, info
 
     def _get_ob(self):
@@ -90,6 +154,9 @@ class FrameStack(gym.Wrapper):
 
 
 class DoneAfterLostLife(gym.Wrapper):
+    """ Reset the game if one life is lost in multiple lifes games.
+        DeepMind trains with this and evaluates without it.
+    """
     def __init__(self, env):
         super(DoneAfterLostLife, self).__init__(env)
 
@@ -153,16 +220,23 @@ class FireResetEnv(gym.Wrapper):
         return self.env.step(ac)
 
 
-def get_wrapped_atari(env_name, mode, **kwargs):
+def get_wrapped_atari(env_name, mode='training', **kwargs):
     """ The preprocessing traditionally used by DeepMind on Atari.
     """
-
     env = gym.make(env_name)
-    env = FireResetEnv(env)
+    assert 'NoFrameskip' in env.spec.id
+
+    if mode == 'training':
+        env = NoopResetEnv(env, noop_max=30)
+
+    env = MaxAndSkipEnv(env, skip=4)
 
     if mode == 'training':
         env = DoneAfterLostLife(env)
         env = SqueezeRewards(env)
+
+    if 'FIRE' in env.unwrapped.get_action_meanings():
+        env = FireResetEnv(env)
 
     env = TransformObservations(env, [
         T.Downsample(84, 84),
