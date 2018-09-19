4360e676bd70b00944855f357f46be90f10fee82
Florin Gogianu <florin.gogianu@gmail.com>
2018-09-19 13:21:28 +0300

    Reconciliate with the new torch API.

--- wintermute/policy_improvement/td_error.py
+++ wintermute/policy_improvement/td_error.py
@@ -2,10 +2,11 @@ import torch
 import torch.nn.functional as F
 
 
-def get_td_error(q_values, q_target_values, rewards, gamma):
+def get_td_error(q_values, q_target_values, rewards, gamma,
+                 reduction='elementwise_mean'):
     """ Compute the temporal difference error.
 
         td_error = (r + gamma * max Q(s_,a)) - Q(s,a)
     """
     expected_q_values = (q_target_values * gamma) + rewards
-    return F.smooth_l1_loss(q_values, expected_q_values)
+    return F.smooth_l1_loss(q_values, expected_q_values, reduction)
