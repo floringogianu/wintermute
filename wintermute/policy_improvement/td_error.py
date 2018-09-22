6d52aae4289069266953862f7111d30334064a9b
Florin Gogianu <florin.gogianu@gmail.com>
2018-09-22 10:50:22 +0300

    Keyword instead of positional argument fix.

--- wintermute/policy_improvement/td_error.py
+++ wintermute/policy_improvement/td_error.py
@@ -9,4 +9,4 @@ def get_td_error(q_values, q_target_values, rewards, gamma,
         td_error = (r + gamma * max Q(s_,a)) - Q(s,a)
     """
     expected_q_values = (q_target_values * gamma) + rewards
-    return F.smooth_l1_loss(q_values, expected_q_values, reduction)
+    return F.smooth_l1_loss(q_values, expected_q_values, reduction=reduction)
