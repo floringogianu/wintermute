8226dc5e240d2763f7bbbf884d71ee707ea4a55a
Tudor Berariu <tudor.berariu@gmail.com>
2018-12-31 20:27:21 +0200

    Experiment with init-methods

--- wintermute/estimators/atari_net.py
+++ wintermute/estimators/atari_net.py
@@ -9,7 +9,14 @@ class AtariNet(nn.Module):
     """ Estimator used for ATARI games.
     """
 
-    def __init__(self, input_channels, hist_len, out_size, hidden_size=256):
+    def __init__(
+        self,
+        input_channels,
+        hist_len,
+        out_size,
+        hidden_size=256,
+        init_method=None,
+    ):
         super(AtariNet, self).__init__()
 
         input_depth = hist_len * input_channels
@@ -25,6 +32,29 @@ class AtariNet(nn.Module):
         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
         self.lin1 = nn.Linear(64 * 7 * 7, hidden_size)
         self.head = nn.Linear(hidden_size, out_size)
+        if init_method is not None:
+            self.reset_weights(init_method)
+
+    def reset_weights(self, init_method: str) -> None:
+        for layer in [self.conv1, self.conv2, self.conv3, self.lin1, self.head]:
+            with torch.no_grad():
+                if layer.bias is not None:
+                    layer.bias.zero_()
+                if init_method == "xavier_uniform":
+                    nn.init.xavier_uniform_(
+                        layer.weight, gain=nn.init.calculate_gain("relu")
+                    )
+                elif init_method == "xavier_normal":
+                    print("hehe")
+                    nn.init.xavier_normal_(
+                        layer.weight, gain=nn.init.calculate_gain("relu")
+                    )
+                elif init_method == "kaiming_uniform":
+                    nn.init.kaiming_uniform_(
+                        layer.weight, mode="fan_in", nonlinearity="relu"
+                    )
+                else:
+                    raise ValueError
 
     def forward(self, x):
         assert (
