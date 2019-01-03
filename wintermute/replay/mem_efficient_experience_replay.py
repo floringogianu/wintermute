e0d45a34b86712756e934f48a5a3458e8da65612
Tudor BERARIU <tberariu@bitdefender.com>
2019-01-03 04:13:10 +0200

    Fix list-based memory

--- wintermute/replay/mem_efficient_experience_replay.py
+++ wintermute/replay/mem_efficient_experience_replay.py
@@ -2,33 +2,14 @@ import numpy.random
 import torch
 
 
-def _collate(batch, batch_size, histlen, mask_dtype=torch.uint8):
-    device = batch[0][0].device
-    frame_size = batch[0][0].size()[2:]
-    states = torch.cat(batch[0], 0).view(batch_size, histlen, *frame_size)
-    actions = torch.tensor(batch[1], device=device, dtype=torch.long).unsqueeze_(1)
-    rewards = torch.tensor(batch[2], device=device, dtype=torch.float).unsqueeze_(1)
-    next_states = torch.cat(batch[3], 0).view(batch_size, histlen, *frame_size)
-    mask = 1 - torch.tensor(batch[4], device=device, dtype=mask_dtype).unsqueeze_(1)
-
-    # if we train with full RGB information (three channels instead of one)
-    if states.ndimension() == 5:
-        n, hist, c, h, w = states.size()
-        states = states.view(n, hist * c, h, w)
-        next_states = next_states.view(n, hist * c, h, w)
-
-    return [states, actions, rewards, next_states, mask]
-
-
 class MemoryEfficientExperienceReplay:
-    """Code is adapted from NaiveExperienceReplay. Same, same but different.
+    """ docstring not found.
     """
 
     def __init__(  # pylint: disable=bad-continuation
         self,
         capacity: int = 100_000,
         batch_size: int = 32,
-        collate=None,
         hist_len: int = 4,
         async_memory: bool = True,
         mask_dtype=torch.uint8,
@@ -42,7 +23,9 @@ class MemoryEfficientExperienceReplay:
         if async_memory:
             import concurrent.futures
 
-            self._executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
+            self._executor = concurrent.futures.ThreadPoolExecutor(
+                max_workers=1
+            )
             self.push = self._push
             self.sample = self._async_sample
             self.push_and_sample = self._async_push_and_sample
@@ -54,62 +37,87 @@ class MemoryEfficientExperienceReplay:
             self.sample = self._sample
             self.push_and_sample = self._push_and_sample
 
-        self._collate = collate or _collate
         self.position = 0
-        self.__new_episode = True
         self.__last_state = None
         self.__mask_dtype = mask_dtype
 
-    def __push_one(self, transition: list):
-        if len(self.memory) < self.capacity:
-            self.memory.append(transition)
-        else:
-            self.memory[self.position] = transition
-        self.position = (self.position + 1) % self.capacity
-
     def _push(self, transition: list) -> None:
         with torch.no_grad():
-            state = transition[0].split(1, 1)
-        if self.__new_episode:
-            for obs in state[:-1]:
-                self.__push_one([obs.clone(), None, None, None])
-        self.__new_episode = done = bool(transition[4])
-        self.__push_one([state[-1].clone(), transition[1], transition[2], done])
+            state = transition[0][:, -1:].clone()
+        to_store = [state, transition[1], transition[2], bool(transition[4])]
+        if len(self.memory) < self.capacity:
+            self.memory.append(to_store)
+        else:
+            self.memory[self.position] = to_store
+        self.position = (self.position + 1) % self.capacity
         self.__last_state = transition[3][:, -1:]
 
     def _sample(self):
         batch = [], [], [], [], []
-        ngood = 0
+        memory = self.memory
         nmemory = len(self.memory)
 
-        while ngood < self.batch_size:
-            idx = numpy.random.randint(0, nmemory)
-            transition = self.memory[idx]
-            if transition[1] is not None:
-                batch[0].append(self.memory[idx - self.histlen + 1][0])
-                for bidx in range(idx - self.histlen + 2, idx):
-                    batch[0].append(self.memory[bidx][0])
-                    batch[3].append(self.memory[bidx][0])
-                batch[0].append(transition[0])
-                batch[1].append(transition[1])
-                batch[2].append(transition[2])
-                batch[3].append(transition[0])
-                batch[4].append(transition[3])
+        for idx in numpy.random.randint(0, nmemory, self.batch_size):
+            transition = last_transition = memory[idx]
+            batch[0].append(transition[0])
+            batch[1].append(transition[1])
+            batch[2].append(transition[2])
+            batch[4].append(transition[3])
+            if idx == self.position - 1:
+                batch[3].append(self.__last_state)
+            else:
+                batch[3].append(self.memory[(idx + 1) % self.capacity][0])
 
-                if idx == self.position - 1:
-                    batch[3].append(self.__last_state.to(batch[0][0].device))
-                elif idx == self.capacity - 1:
-                    batch[3].append(self.memory[0][0])
-                else:
-                    batch[3].append(self.memory[idx + 1][0])
+            found_done = False
+            bidx = idx
+            for _ in range(self.histlen - 1):
+                bidx = (bidx - 1) % self.capacity
+                batch[3].append(batch[0][-1])
+                if not found_done:
+                    new_transition = memory[bidx]
+                    if new_transition[3]:
+                        found_done = True
+                    else:
+                        last_transition = new_transition
+                batch[0].append(last_transition[0])
 
-                ngood += 1
         return self._collate(
             batch, self.batch_size, self.histlen, mask_dtype=self.__mask_dtype
         )
 
+    def _collate(self, batch, batch_size, histlen, mask_dtype=torch.uint8):
+        device = batch[0][0].device
+        frame_size = batch[0][0].size()[2:]
+        states = torch.cat(batch[0][::-1], 0).view(
+            batch_size, histlen, *frame_size
+        )
+        actions = torch.tensor(  # pylint: disable=E1102
+            batch[1][::-1], device=device, dtype=torch.long
+        ).unsqueeze_(1)
+        rewards = torch.tensor(  # pylint: disable=E1102
+            batch[2][::-1], device=device, dtype=torch.float
+        ).unsqueeze_(1)
+        next_states = torch.cat(batch[3][::-1], 0).view(
+            batch_size, histlen, *frame_size
+        )
+        mask = 1 - torch.tensor(  # pylint: disable=E1102
+            batch[4][::-1], device=device, dtype=mask_dtype
+        ).unsqueeze_(1)
+
+        # if we train with full RGB information (three channels instead of one)
+        if states.ndimension() == 5:
+            bsz, hist, nch, h, w = states.size()
+            states = states.view(bsz, hist * nch, h, w)
+            next_states = next_states.view(bsz, hist * nch, h, w)
+
+        return [states, actions, rewards, next_states, mask]
+
     def _push_and_sample(self, transition: list):
-        self._push(transition)
+        if isinstance(transition[0], list):
+            for trans in transition:
+                self._push(trans)
+        else:
+            self._push(transition)
         return self._sample()
 
     # -- Async versions
@@ -155,7 +163,9 @@ class MemoryEfficientExperienceReplay:
         else:
             batch = self._sample()
 
-        self._sample_result = self._executor.submit(self._push_and_sample, transition)
+        self._sample_result = self._executor.submit(
+            self._push_and_sample, transition
+        )
         return batch
 
     def __len__(self):
