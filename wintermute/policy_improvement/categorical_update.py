82bf15bfbb131d1e17c121f9d78519e752d2e73a
Florin Gogianu <florin.gogianu@gmail.com>
2019-06-27 11:25:46 +0300

    Refactor Categorical Update, making the API closer to the DQN update.

--- wintermute/policy_improvement/categorical_update.py
+++ wintermute/policy_improvement/categorical_update.py
@@ -5,7 +5,135 @@ import torch
 from wintermute.utils import get_estimator_device, to_device, DQNLoss
 
 
-__all__ = ["CategoricalPolicyImprovement"]
+__all__ = [
+    "CategoricalPolicyImprovement",
+    "get_categorical_loss",
+    "get_target_distribution",
+]
+
+
+def get_target_distribution(
+    next_states, rewards, mask, gamma, target_estimator, support
+):
+    r""" Computes the target distribution TZ(x_,a).
+
+    The size of `next_states` can be smaller than that of the actual
+    batch size to save computation.
+    """
+    bsz = rewards.shape[0]
+    bsz_ = next_states.shape[0]
+    bin_no = support.shape[0]
+    v_min, v_max = support[0].item(), support[-1].item()
+    delta_z = (v_max - v_min) / (bin_no - 1)
+
+    probs = target_estimator(next_states)
+    qs = torch.mul(probs, support.expand_as(probs))
+    argmax_a = qs.sum(2).max(1)[1].unsqueeze(1).unsqueeze(1)
+    action_mask = argmax_a.expand(bsz_, 1, bin_no)
+    _qa_probs = probs.gather(1, action_mask).squeeze()
+
+    # Next-states batch can be smaller so we scatter qa_probs in
+    # a tensor the size of the full batch with each row summing to 1
+    qa_probs = torch.eye(bsz, bin_no, device=_qa_probs.device)
+    qa_probs.masked_scatter_(mask.expand_as(qa_probs), _qa_probs)
+
+    # Mask gamma and reshape it torgether with rewards to fit p(x,a).
+    rewards = rewards.expand_as(qa_probs)
+    gamma = (mask.float() * gamma).expand_as(qa_probs)
+
+    # Compute projection of the application of the Bellman operator.
+    bellman_op = rewards + gamma * support.unsqueeze(0).expand_as(rewards)
+    bellman_op = torch.clamp(bellman_op, v_min, v_max)
+
+    # Compute categorical indices for distributing the probability
+    m = torch.zeros(bsz, bin_no, device=qa_probs.device)
+    b = (bellman_op - v_min) / delta_z
+    l = b.floor().long()
+    u = b.ceil().long()
+
+    # Fix disappearing probability mass when l = b = u (b is int)
+    l[(u > 0) * (l == u)] -= 1
+    u[(l < (bin_no - 1)) * (l == u)] += 1
+
+    # Distribute probability
+    """
+    for i in range(bsz):
+        for j in range(self.bin_no):
+            uidx = u[i][j]
+            lidx = l[i][j]
+            m[i][lidx] = m[i][lidx] + qa_probs[i][j] * (uidx - b[i][j])
+            m[i][uidx] = m[i][uidx] + qa_probs[i][j] * (b[i][j] - lidx)
+    for i in range(bsz):
+        m[i].index_add_(0, l[i], qa_probs[i] * (u[i].float() - b[i]))
+        m[i].index_add_(0, u[i], qa_probs[i] * (b[i] - l[i].float()))
+    """
+    # Optimized by https://github.com/tudor-berariu
+    offset = (
+        torch.linspace(0, ((bsz - 1) * bin_no), bsz, device=qa_probs.device)
+        .long()
+        .unsqueeze(1)
+        .expand(bsz, bin_no)
+    )
+
+    m.view(-1).index_add_(
+        0, (l + offset).view(-1), (qa_probs * (u.float() - b)).view(-1)
+    )
+    m.view(-1).index_add_(
+        0, (u + offset).view(-1), (qa_probs * (b - l.float())).view(-1)
+    )
+    return m, probs
+
+
+def get_categorical_loss(
+    batch, estimator, gamma, support, target_estimator=None
+):
+    r""" Computes the Categorical KL loss phi(TZ(x_,a)) || Z(x,a).
+
+        Largely analogue to the DQN loss routine.
+
+        Args:
+            batch (list): The (states, actions, rewards, next_states, done_mask)
+                batch.
+            estimator (nn.Module): The *online* estimator.
+            gamma (float): Discount factor γ.
+            support (torch.tensor): A vector of size `bin_no` that is the
+                support of the categorical distribution.
+            target_estimator (nn.Module, optional): Defaults to None. The target
+                estimator. If None the target is computed using the online
+                estimator.
+
+        Returns:
+            DQNLoss: A simple namespace containing the loss and its byproducts.
+    """
+    assert target_estimator is not None, "Online target not implemented yet."
+
+    states, actions, rewards, next_states, mask = batch
+    bsz = states.shape[0]
+    bin_no = support.shape[0]
+
+    # Compute probability distribution of Q(s, a)
+    qs_probs = estimator(states)
+    action_mask = actions.view(bsz, 1, 1).expand(bsz, 1, bin_no)
+    qsa_probs = qs_probs.gather(1, action_mask).squeeze()
+
+    # Compute probability distribution of Q(s_, a)
+    with torch.no_grad():
+        target_qsa_probs, target_qs_probs = get_target_distribution(
+            next_states, rewards, mask, gamma, target_estimator, support
+        )
+
+    # Compute the cross-entropy of phi(TZ(x_,a)) || Z(x,a) for each
+    # transition in the batch
+    qsa_probs = qsa_probs.clamp(min=1e-7)  # Tudor's trick for avoiding nans
+    loss = -(target_qsa_probs * torch.log(qsa_probs)).sum(dim=1)
+
+    return DQNLoss(
+        loss=loss,
+        qsa=qsa_probs,
+        qsa_targets=target_qsa_probs,
+        q_values=(support, qs_probs),
+        q_targets=(support, target_qs_probs),
+    )
 
 
 class CategoricalPolicyImprovement:
@@ -25,7 +153,6 @@ class CategoricalPolicyImprovement:
         self.gamma = gamma
         self.support = torch.linspace(*support, device=self.device)
         self.v_min, self.v_max, self.bin_no = support
-        self.delta_z = (self.v_max - self.v_min) / (self.bin_no - 1)
         if target_estimator is True:
             self.target_estimator = deepcopy(estimator)
         else:
@@ -33,31 +160,15 @@ class CategoricalPolicyImprovement:
 
     def __call__(self, batch, cb=None):
         batch = to_device(batch, self.device)
-        states, actions, rewards, next_states, mask = batch
-        bsz = states.shape[0]
 
-        # Compute probability distribution of Q(s, a)
-        qs_probs = self.estimator(states)
-        action_mask = actions.view(bsz, 1, 1).expand(bsz, 1, self.bin_no)
-        qsa_probs = qs_probs.gather(1, action_mask).squeeze()
-
-        # Compute probability distribution of Q(s_, a)
-        with torch.no_grad():
-            target_qsa_probs = self._get_target_distribution(
-                next_states, rewards, mask
-            )
-
-        # Compute the cross-entropy of phi(TZ(x_,a)) || Z(x,a)
-        qsa_probs = qsa_probs.clamp(min=1e-7)  # Tudor's trick for avoiding nans
-        loss = -torch.sum(target_qsa_probs * torch.log(qsa_probs))
-
-        loss = DQNLoss(
-            loss=loss,
-            qsa=qsa_probs,
-            qsa_targets=target_qsa_probs,
-            q_values=self.support,
-            q_targets=self.support,
+        loss = get_categorical_loss(
+            batch,
+            self.estimator,
+            self.gamma,
+            self.support,
+            target_estimator=self.target_estimator,
         )
+
         if cb:
             loss = cb(loss)
         else:
@@ -65,77 +176,6 @@ class CategoricalPolicyImprovement:
 
         loss.backward()
         self.update_estimator()
-
-    def _get_target_distribution(self, next_states, rewards, mask):
-        r""" Computes the target distribution.
-
-        The size of `next_states` can be smaller than that of the actual
-        batch size to save computation.
-        """
-        bsz = rewards.shape[0]
-        bsz_ = next_states.shape[0]
-
-        probs = self.target_estimator(next_states)
-        qs = torch.mul(probs, self.support.expand_as(probs))
-        argmax_a = qs.sum(2).max(1)[1].unsqueeze(1).unsqueeze(1)
-        action_mask = argmax_a.expand(bsz_, 1, self.bin_no)
-        _qa_probs = probs.gather(1, action_mask).squeeze()
-
-        # Next-states batch can be smaller so we scatter qa_probs in
-        # a tensor the size of the full batch with each row summing to 1
-        qa_probs = torch.eye(bsz, self.bin_no, device=self.device)
-        qa_probs.masked_scatter_(mask.expand_as(qa_probs), _qa_probs)
-
-        # Mask gamma and reshape it torgether with rewards to fit p(x,a).
-        rewards = rewards.expand_as(qa_probs)
-        gamma = (mask.float() * self.gamma).expand_as(qa_probs)
-
-        # Compute projection of the application of the Bellman operator.
-        bellman_op = rewards + gamma * self.support.unsqueeze(0).expand_as(
-            rewards
-        )
-        bellman_op = torch.clamp(bellman_op, self.v_min, self.v_max)
-
-        # Compute categorical indices for distributing the probability
-        m = torch.zeros(bsz, self.bin_no, device=self.device)
-        b = (bellman_op - self.v_min) / self.delta_z
-        l = b.floor().long()
-        u = b.ceil().long()
-
-        # Fix disappearing probability mass when l = b = u (b is int)
-        l[(u > 0) * (l == u)] -= 1
-        u[(l < (self.bin_no - 1)) * (l == u)] += 1
-
-        # Distribute probability
-        """
-        for i in range(bsz):
-            for j in range(self.bin_no):
-                uidx = u[i][j]
-                lidx = l[i][j]
-                m[i][lidx] = m[i][lidx] + qa_probs[i][j] * (uidx - b[i][j])
-                m[i][uidx] = m[i][uidx] + qa_probs[i][j] * (b[i][j] - lidx)
-        for i in range(bsz):
-            m[i].index_add_(0, l[i], qa_probs[i] * (u[i].float() - b[i]))
-            m[i].index_add_(0, u[i], qa_probs[i] * (b[i] - l[i].float()))
-        """
-        # Optimized by https://github.com/tudor-berariu
-        offset = (
-            torch.linspace(
-                0, ((bsz - 1) * self.bin_no), bsz, device=self.device
-            )
-            .long()
-            .unsqueeze(1)
-            .expand(bsz, self.bin_no)
-        )
-
-        m.view(-1).index_add_(
-            0, (l + offset).view(-1), (qa_probs * (u.float() - b)).view(-1)
-        )
-        m.view(-1).index_add_(
-            0, (u + offset).view(-1), (qa_probs * (b - l.float())).view(-1)
-        )
-
-        return m
 
     def update_estimator(self):
         r""" Do the estimator optimization step. Usefull when computing
@@ -197,10 +237,10 @@ def main():
     net = Net(action_no, support[2])
     y = net(x)
 
-    pi = CategoricalPolicyImprovement(
-        net, optim.Adam(net.parameters()), 0.92, support
+    print(
+        "target p(Q(s_,a)):\n",
+        get_target_distribution(x, rewards, mask, 0.92, net, support),
     )
-    print("target p(Q(s_,a)):\n", pi._get_target_distribution(x, rewards, mask))
 
 
 if __name__ == "__main__":
